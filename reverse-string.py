fd4974555062a5f0419a3021968b7ad66f378674
Chris Wu <eph@EphdeMacBook-Pro.local>
2019-05-28 20:01:20 +0800

    add words-in-a-string.py

--- reverse-string.py
+++ reverse-string.py
@@ -1,4 +1,24 @@
-#https://leetcode.com/problems/reverse-string/
+"""
+Reversing a string is equivalent to
+Switch the first and the last, the second and the second last, the third and the third last...
+So I use two pointers, 's' at the start, 'e' at the end.
+If e and s collapse, the proccess is finished.
+
+(a, b) = (b, a) is equal to
+
+temp = a
+a = b
+b = temp
+
+For time complexity is O(N), N is the length of the string.
+Space complexity is O(1), because I only use pointers and a space to store one char.
+"""
 class Solution(object):
-    def reverseString(self, s):
-        return s[::-1]
+    def reverseString(self, string):
+        s = 0
+        e = len(string)-1
+        while e>s:
+            (string[s], string[e]) = (string[e], string[s])
+            s+=1
+            e-=1
+        return string
---
+++ reverse-words-in-a-string.py
@@ -0,0 +1,27 @@
+"""
+I see space as the end of a word.
+If a word end I will add it to the left of the output.
+That is why I have to add an extra space to the original 's' [0]
+Be careful There may be continuous space or space at the start and the end. [1]
+The way we combine string, there will be an extra space at the end, so I remove it. [2]
+
+Time complexity is O(N).
+Because we loop through the stirng once.
+
+Space complexity is O(N).
+Because we use 'opt' to store the output.
+"""
+class Solution(object):
+    def reverseWords(self, s):
+        temp = ''
+        opt = ''
+        s = s+' ' #[0]
+        for c in s:
+            if c==' ':
+                if temp is not '': opt = temp+' '+opt #[1]
+                temp = ''
+            else:
+                temp+=c
+
+        opt = opt[:-1] #[2]
+        return opt
