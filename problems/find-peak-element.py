ef355bc386d6354b9f3c81e8b63ac8e52bed7a9f
Chris Wu <eph@EphdeMacBook-Pro.local>
2019-08-26 21:01:28 +0800

    update answer

--- problems/find-peak-element.py
+++ problems/find-peak-element.py
@@ -5,8 +5,9 @@ We move `l` to `p+1`, since the peak must be at `p+1` or the right of the `p+1`.
 Else, it means the `p` and `p+1` is at the right side of the mountain,
 We move the `r` to `p`, since the peak must be at `p` or the left of the `p`.
 The `l` and `r` will move closer and closer to the peak until they meet together.
-The time complexity is O(LogN)
+The time complexity is `O(LogN)`
 """
+#O(LogN) Solution
 class Solution(object):
     def findPeakElement(self, nums):
         l = 0
