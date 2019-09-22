3735871fadc48f375eebb189bdb21b9aea97f8c1
Chris Wu <eph@EphdeMacBook-Pro.local>
2019-09-22 21:52:24 +0800

    no message

---
+++ problems/combination-sum-iii.py
@@ -0,0 +1,27 @@
+"""
+If you haven't seen [this problem](https://leetcode.com/problems/combinations/), I suggest you do that first!
+And here is the [explaination](https://leetcode.com/problems/combinations/discuss/387753) for that problem.
+
+This problem is basically the same as [that problem](https://leetcode.com/problems/combinations/) with its N=9.
+The difference is we have to find the combination that sum up as N.
+So all we have to do is change the condition of [0]. And the condition for backtracking [1].
+"""
+class Solution(object):
+    def combinationSum3(self, K, N):
+        answer = []
+        ans = []
+        first = 1
+        total = 0
+        while True:
+            if len(ans)==K and total==N: #[0]
+                answer.append(ans[:])
+
+            if len(ans)==K or total>N or first>9: #[1]
+                if not ans: return answer
+                first = ans.pop() #backtrack
+                total-=first
+                first+=1
+            else:
+                ans.append(first)
+                total+=first
+                first+=1
