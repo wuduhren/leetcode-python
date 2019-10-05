6ac9019e5dedab21716cc043b549b3d44d44f785
Chris Wu <eph@EphdeMacBook-Pro.local>
2019-10-05 22:09:35 +0800

    no message

--- problems/3sum.py
+++ problems/3sum.py
@@ -1,43 +1,40 @@
+"""
+This is the answer from @caikehe and all the comments below
+
+The main idea is to iterate every number in nums.
+We use the number as a target to find two other numbers which make total zero.
+For those two other numbers, we move pointers, `l` and `r`, to try them.
+
+`l` start from left to right.
+`r` start from right to left.
+
+First, we sort the array, so we can easily move i around and know how to adjust l and r.
+If the number is the same as the number before, we have used it as target already, continue. [1]
+We always start the left pointer from `i+1` because the combination of 0~`i` has already been tried. [2]
+
+Now we calculate the total:
+If the total is less than zero, we need it to be larger, so we move the left pointer. [3]
+If the total is greater than zero, we need it to be smaller, so we move the right pointer. [4]
+If the total is zero, bingo! [5]
+We need to move the left and right pointers to the next different numbers, so we do not get repeating result. [6]
+
+We do not need to consider `i` after `nums[i]>0`, since sum of 3 positive will be always greater than zero. [7]
+We do not need to try the last two, since there are no rooms for `l` and `r` pointers.
+You can think of it as The last two have been tried by all others. [8]
+
+For time complexity
+Sorting takes `O(NlogN)`
+Now, we need to think as if the `nums` is really really big
+We iterate through the `nums` once, and each time we iterate the whole array again by a while loop
+So it is `O(NlogN+N^2)~=O(N^2)`
+
+For space complexity
+We didn't use extra space except the `res`
+Since we may store the whole 'nums' in it
+So it is `O(N)`
+`N` is the length of `nums`
+"""
 class Solution(object):
-	#this is the answer from caikehe and all the comments below
-
-	"""
-	This is the answer from caikehe and all the comments below
-
-	The main idea is to iterate every number in nums.
-	We use the number as a target to find two other numbers which make total zero.
-	For those two other numbers, we move pointers, l and r, to try them.
-
-	l start from left to right
-	r start from right to left
-
-	First, we sort the array, so we can easily move i around and know how to adjust l and r.
-	If the number is the same as the number before, we have used it as target already, continue. [1]
-	We always start the left pointer from i+1 because the combination of 0~i has already been tried. [2]
-
-	Now we calculate the total:
-	If the total is less than zero, we need it to be larger, so we move the left pointer. [3]
-	If the total is greater than zero, we need it to be smaller, so we move the right pointer. [4]
-	If the total is zero, bingo! [5]
-	We need to move the left and right pointers to the next different numbers, so we do not get repeating result. [6]
-
-	We do not need to consider i after nums[i]>0, since sum of 3 positive will be always greater than zero. [7]
-	We do not need to try the last two, since there are no rooms for l and r pointers.
-	You can think of it as The last two have been tried by all others. [8]
-
-	For time complexity
-	Sorting takes O(NlogN)
-	Now, we need to think as if the 'nums' is really really big
-	We iterate through the 'nums' once, and each time we iterate the whole array again by a while loop
-	So it is O(NlogN+N^2)~=O(N^2)
-
-	For space complexity
-	We didn't use extra space except the 'res'
-	Since we may store the whole 'nums' in it
-	So it is O(N)
-	N is the length of 'nums'
-	"""
-
 	def threeSum(self, nums):
 		res = []
 		nums.sort()
@@ -63,7 +60,7 @@ class Solution(object):
 					l+=1
 					r-=1
 		return res
-		
+
 	"""
     def threeSum(self, nums):
         def twoSum(target, nums):
@@ -77,12 +74,12 @@ class Solution(object):
                 seen[n2]+=1
                 res.append([target*-1, n1, n2])
             return res
-        
+
         res = []
         zero = 0
         positive = collections.Counter()
         negative = collections.Counter()
-        
+
         for n in nums:
             if n>0:
                 positive[n]+=1
@@ -90,18 +87,18 @@ class Solution(object):
                 zero+=1
             else:
                 negative[n]+=1
-                
+
         if zero>=3:
             res.append([0, 0, 0])
         if zero>=1:
             for p in positive:
                 if p*-1 in negative:
                     res.append([0, p, p*-1])
-                
+
         for p in positive:
             res+=twoSum(p*-1, negative)
         for n in negative:
             res+=twoSum(n*-1, positive)
-            
+
         return res
     """
