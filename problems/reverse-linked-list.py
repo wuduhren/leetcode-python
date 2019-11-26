ae5228865698f02f82aaa2c7f97fc804c80056e2
Chris Wu <eph@EphdeMacBook-Pro.local>
2019-11-26 22:38:43 +0800

    no message

--- problems/reverse-linked-list.py
+++ problems/reverse-linked-list.py
@@ -1,24 +1,45 @@
 #https://leetcode.com/problems/reverse-linked-list/
+# class Solution(object):
+#     #iterative
+#     def reverseList(self, head):
+#         prev = None
+#         current = head
+#         while current:
+#             temp = current.next
+#             current.next = prev
+#             prev = current
+#             current = temp
+
+#         return prev
+
+#     #recursive
+#     def reverseList(self, head):
+#         if head is None or head.next is None:
+#             return head
+
+#         new_head = self.reverseList(head.next)
+#         n = head.next
+#         n.next = head
+#         head.next = None
+#         return new_head
+
+#recursive
 class Solution(object):
-    #iterative
-    def reverseList(self, head):
-        prev = None
-        current = head
-        while current:
-            temp = current.next
-            current.next = prev
-            prev = current
-            current = temp
+    def reverseList(self, node):
+        if node and node.next:
+            new_head = self.reverseList(node.next)
+            node.next.next = node
+            node.next = None
+            return new_head
+        return node
 
-        return prev
-
-    #recursive
-    def reverseList(self, head):
-        if head is None or head.next is None:
-            return head
-
-        new_head = self.reverseList(head.next)
-        n = head.next
-        n.next = head
-        head.next = None
-        return new_head
+#iterative
+class Solution(object):
+    def reverseList(self, node):
+        pre = None
+        while node:
+            next_node = node.next
+            node.next = pre
+            if not next_node: return node
+            pre = node
+            node = next_node
