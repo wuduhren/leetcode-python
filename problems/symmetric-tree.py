ce8a3868316678dbc7f83c521dde1fc6c0b76128
Chris Wu <wu.duhren@verizonmedia.com>
2020-04-06 11:08:13 +0800

    no message

--- problems/symmetric-tree.py
+++ problems/symmetric-tree.py
@@ -16,3 +16,21 @@ class Solution(object):
                 q.append((left_node.right, right_node.left))
                 q.append((left_node.left, right_node.right))
         return True
+
+
+# class Solution(object):
+#     def isSymmetric(self, root):
+#         if not root: return True
+        
+#         stack = []
+#         stack.append((root.left, root.right))
+#         while stack:
+#             left_node, right_node = stack.pop()
+#             if not left_node and right_node: return False
+#             if not right_node and left_node: return False
+            
+#             if left_node and right_node:
+#                 if left_node.val!=right_node.val: return False
+#                 stack.append((left_node.right, right_node.left))
+#                 stack.append((left_node.left, right_node.right))
+#         return True
