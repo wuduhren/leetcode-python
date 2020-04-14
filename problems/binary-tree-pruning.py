e6e3547f176491882b906c6cf3f2c0dd31d3fa22
Chris Wu <wu.duhren@verizonmedia.com>
2020-04-15 07:03:29 +0800

    no message

---
+++ problems/binary-tree-pruning.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def pruneTree(self, root):
+        def hasOne(node):
+            if not node: return False
+
+            left_has_one = hasOne(node.left)
+            right_has_one = hasOne(node.right)
+
+            if not left_has_one: node.left = None
+            if not right_has_one: node.right = None
+
+            return node.val==1 or left_has_one or right_has_one
+        
+        return root if hasOne(root) else None
+
+class Solution(object):
+    def pruneTree(self, node):
+        if not node: return None
+        node.left = self.pruneTree(node.left)
+        node.right = self.pruneTree(node.right)
+        if not node.left and not node.right and node.val==0: return None
+        return node
+
+"""
+Time complexity is O(N). Because we traverse all the nodes.
+Space complexity is O(LogN). The tree's height is about O(LogN), so we have O(LogN) level of recursion and used up O(LogN) of stack memory
+"""
