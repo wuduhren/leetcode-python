11a12fb930014f74056f02d3177ed95114c6efd2
Chris Wu <wu.duhren@verizonmedia.com>
2021-05-16 15:12:43 +0800

    no message

--- problems/stone-game-ii.py
+++ problems/stone-game-ii.py
@@ -6,12 +6,17 @@ class Solution(object):
             if start>=len(piles): return 0
             if start+m*2>=len(piles): return sum(piles[start:])
             
-            stones = float('-inf')
+            max_stones = float('-inf')
             for x in xrange(1, m*2+1):
-                stones = max(stones, sum(piles[start:])-helper(start+x, max(m, x)))
+                max_stones = max(max_stones, sum(piles[start:])-helper(start+x, max(m, x)))
             
-            history[(start, m)] = stones
+            history[(start, m)] = max_stones
             return history[(start, m)]
         
         history = {}
         return helper(0, 1)
+
+"""
+helper(start, m) := the max stones that the first player will get with given piles[start:] and M.
+max_stones = MAX{ (sum of all the stones) - (max_stones the other player will get) } = MAX{ sum(piles[start:]) - helper(start+x, max(m, x)) }
+"""
