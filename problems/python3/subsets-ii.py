06dd433f726e47c155d77dd3524fc57316a67676
wuduhren <wuduhren@outlook.com>
2022-06-02 14:13:23 +0800

    subsets-ii

---
+++ problems/python3/subsets-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        def helper(i):
+            if i==len(nums):
+                ans.append(subset.copy())
+                return
+            
+            subset.append(nums[i])
+            helper(i+1)
+            subset.pop()
+            
+            while i+1<len(nums) and nums[i]==nums[i+1]: i += 1
+            helper(i+1)
+            
+        nums.sort()
+        ans = []
+        subset = []
+        helper(0)
+        return ans
